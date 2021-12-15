9d4f5b82266985fe4f262006374a24a82989cbbd
encorey <lukasz.jagodka@protonmail.com>
2021-12-15 20:36:19 +0100

    added fetch functions and refac. otherFunctions

--- src/helpers/logout.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const logOut = () => {
-  localStorage.removeItem('selectedAccount');
-  localStorage.removeItem('currencesFromApi');
-  const { port } = window.location;
-  window.location.href = `http://localhost:${port}/`;
-};
--- src/helpers/validationAccount.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const matchExpression = (str: any) => {
-  const rgularExp = {
-    onlyNumbers: /^[0-9]+$/,
-  };
-  return rgularExp.onlyNumbers.test(str);
-};
