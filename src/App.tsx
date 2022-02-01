b1d1b1b7e93df1b4544706a18f9e22ec4eb54f12
encorey <kosz.kosz@protonmail.com>
2022-02-01 18:06:25 +0100

    Add fetchAllAccounts dispatch in files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Dashboard from './components/dashboard/Routes';
 import { fetchRates } from './helpers/fetchRates';
 import './App.css';
+import { fetchAllAccounts } from './components/accounts/accountsSlice';
+import { IAccount } from './types/types';
 
 class App extends Component {
   componentDidMount() {
     fetchRates();
+    fetchAllAccounts();
   }
 
   render() {
@@ -17,4 +21,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch: any) => ({
+  fetchAllAccounts: () => dispatch(fetchAllAccounts()),
+});
+
+export default connect(null, mapDispatchToProps)(App);
