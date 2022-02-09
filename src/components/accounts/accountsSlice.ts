a9d73a6fbace955639eb371cf6e3d16b08cefbed
encorey <kosz.kosz@protonmail.com>
2022-02-09 19:36:57 +0100

    Refactor accounts/removeAccount redux action

--- src/components/accounts/accountsSlice.ts
+++ src/components/accounts/accountsSlice.ts
@@ -2,9 +2,10 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-console */
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { IAccount, TFetchAccounts, TPair } from '../../types/types';
+import { IAccount, TFetchAccounts } from '../../types/types';
 import { get } from '../../helpers/fetch/get';
 import { post } from '../../helpers/fetch/post';
+import { deleteAccount } from '../../helpers/fetch/delete';
 
 interface IState {
   accounts: Array<IAccount>,
@@ -52,17 +53,22 @@ export const fetchAccounts = createAsyncThunk(
   },
 );
 
+export const removeAccount = createAsyncThunk<void, number, {state: IState }>(
+  'accounts/removeAccount',
+  async (id, { getState }) => {
+    const { accounts } = getState();
+    deleteAccount(`accounts?id=${id}`);
+    const index = accounts.findIndex((account) => account.id === id);
+    if (index !== -1) { accounts.splice(index, 1); }
+  },
+);
+
 export const accountsSlice = createSlice({
   name: 'accounts',
   initialState,
   reducers: {
     createAccount: ({ accounts }, { payload }: PayloadAction<Partial<IAccount>>) => {
       post(payload, 'accounts');
-    },
-    removeAccount: ({ accounts }, { payload }: PayloadAction<any>) => {
-      post(payload, 'accounts/deleteaccount');
-      const index = accounts.findIndex((account) => account.id === payload);
-      if (index !== -1) { accounts.splice(index, 1); }
     },
     selectAccount: (state, { payload }: PayloadAction<IAccount>) => {
       localStorage.setItem('selectedAccount', JSON.stringify(payload));
@@ -112,7 +118,6 @@ export const accountsSlice = createSlice({
 
 export const {
   createAccount,
-  removeAccount,
   selectAccount,
   updateAccountValue,
 } = accountsSlice.actions;
